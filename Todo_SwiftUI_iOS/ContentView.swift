8e556188638a87b53d583feda1a943750a60ab61
Demetrius Vissarion <demetrius.vissarion@gmail.com>
2024-02-13 17:38:35 +0000

    step_05

--- Todo_SwiftUI_iOS/ContentView.swift
+++ Todo_SwiftUI_iOS/ContentView.swift
@@ -8,61 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var timesBeingTapped = 0
-    @State private var petName = ""
+    @State private var todo1 = "Todo 1"
+    @State private var todo2 = "Todo 2"
+    @State private var todo3 = "Todo 3"
+    @State private var todo4 = "Todo 4"
+    @State private var todo5 = "Todo 5"
     @State private var isToggle1On: Bool = false
     @State private var isToggle2On: Bool = false
     @State private var isToggle3On: Bool = false
     @State private var isToggle4On: Bool = false
     @State private var isToggle5On: Bool = false
-    
+
     var body: some View {
         VStack {
             Image("backgroundImage")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 350, height: 250)
+            
             HStack {
                 Text("Todos!")
                     .font(.system(size: 52, weight: .light, design: .default))
                     .foregroundColor(.black)
-                    
-
+                
+                Spacer()
+                
                 Text("Toggle all them off!")
                     .foregroundColor(.black)
             }
-
-            VStack {
-                Toggle(isOn: $isToggle1On) {
-                    Text("Todo 1")
+            .padding(.horizontal)
+            
+            Form {
+                HStack {
+                    TextField("Todo 1", text: $todo1)
+                    Spacer()
+                    Toggle("", isOn: $isToggle1On)
                 }
-                Toggle(isOn: $isToggle2On) {
-                    Text("Todo 2")
+                HStack {
+                    TextField("Todo 2", text: $todo2)
+                    Spacer()
+                    Toggle("", isOn: $isToggle2On)
                 }
-                Toggle(isOn: $isToggle3On) {
-                    Text("Todo 3")
+                HStack {
+                    TextField("Todo 3", text: $todo3)
+                    Spacer()
+                    Toggle("", isOn: $isToggle3On)
                 }
-                Toggle(isOn: $isToggle4On) {
-                    Text("Todo 4")
+                HStack {
+                    TextField("Todo 4", text: $todo4)
+                    Spacer()
+                    Toggle("", isOn: $isToggle4On)
                 }
-                Toggle(isOn: $isToggle5On) {
-                    Text("Todo 5")
+                HStack {
+                    TextField("Todo 5", text: $todo5)
+                    Spacer()
+                    Toggle("", isOn: $isToggle5On)
                 }
             }
             .foregroundColor(.black)
         }
-        .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(Color.white)
-        
-        Button("Times tapped: \(timesBeingTapped)") {
-            timesBeingTapped += 1
-        }
-        
-        Form {
-            TextField("Enter your pet's name", text: $petName)
-            Text("Hello, \(petName)!")
-        }
     }
 }
 
